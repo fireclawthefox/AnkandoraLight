086ecf8e980f0f7f23397492f76661023c04546f
Fireclaw <fireclawthefox@gmail.com>
2021-10-21 16:45:02 +0200

    Updated win/loose messages

--- src/room/DRoom.py
+++ src/room/DRoom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e934473e9abdc6ff6fa1e0fa4356f3685a8826089eb488e59ec50ad1870cb16b
-size 3101
+oid sha256:ddea465c448f53a88cdfd8f04c14891ef4f7f4d1708ff90847171ce7efc8f397
+size 3198
