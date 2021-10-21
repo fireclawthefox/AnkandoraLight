21eae0ae423384091c9ab8fdbe51836a282a3de9
Fireclaw <fireclawthefox@gmail.com>
2021-10-21 16:26:36 +0200

    Fix battle initiative calculation
    - added missing special ability addition

--- src/battle/DBattleAI.py
+++ src/battle/DBattleAI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:025148cc20c8d0bacc60410d2de2d270c5d96190d1402f6df31be3e3a2e496d8
-size 12564
+oid sha256:79a9a036318ec24081ec42b13154f10e58dfa717c7689e79912ad9c876a95077
+size 12574
