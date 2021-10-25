07d2fd438d70e206f1ae90d3f90a577ca4e32320
Fireclaw <fireclawthefox@gmail.com>
2021-10-25 16:59:10 +0200

    Updated loose condition text

--- src/battle/DBattle.py
+++ src/battle/DBattle.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8bcfccdb12bbe7d02f318555c2510100e2df55ce18ce9cf9f3e1657a011d0b
-size 2820
+oid sha256:deffabae1e8840dcae0e2167f34ba08b25422e86778ef10e6e38e5a5d6919922
+size 2850
