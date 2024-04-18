61a56ba263371bb69af7b11bc9c07d295e510cd6
Fireclaw <fireclawthefox@gmail.com>
2024-04-18 21:07:45 +0200

    Add explicit module includes for server app in setup.py

--- src/setup.py
+++ src/setup.py
@@ -40,6 +40,37 @@ setup(
                     "direct.distributed.TimeManagerAI",
                     "direct.distributed.DistributedNodeAI",
                     "direct.distributed.DistributedSmoothNodeAI"
+                ],
+                "AnkandoraServer": [
+                    "direct.particles.ParticleManagerGlobal",
+                    "direct.showbase.PhysicsManagerGlobal",
+
+                    "chat.DMessage",
+                    "roomManager.DRoomManager",
+                    "board.DBoard",
+                    "questCard.DQuestCard",
+                    "room.DRoom",
+                    "player.DBotPlayer",
+                    "player.DPlayer",
+                    "piece.DPiece",
+                    "battle.DBattle",
+                    "roomManager.DRoomManagerAI",
+                    "board.DBoardAI",
+                    "questCard.DQuestCardAI",
+                    "room.DRoomAI",
+                    "player.DBotPlayerAI",
+                    "player.DPlayerAI",
+                    "piece.DPieceAI",
+                    "battle.DBattleAI",
+
+                    "direct.distributed.DistributedObject",
+                    "direct.distributed.TimeManager",
+                    "direct.distributed.DistributedNode",
+                    "direct.distributed.DistributedSmoothNode",
+                    "direct.distributed.DistributedObjectAI",
+                    "direct.distributed.TimeManagerAI",
+                    "direct.distributed.DistributedNodeAI",
+                    "direct.distributed.DistributedSmoothNodeAI"
                 ]
             }
         }
